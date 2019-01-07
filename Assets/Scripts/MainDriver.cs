5b5fdb5823344fa249af0a1b3047012e5cb641f1
Jake Mauk <larcondos@yahoo.com>
2019-01-07 14:01:26 -0500

    Documented all code.

--- Assets/Scripts/MainDriver.cs
+++ Assets/Scripts/MainDriver.cs
@@ -6,22 +6,41 @@ using UnityEngine.UI;
 
 public class MainDriver : MonoBehaviour {
 
+	#region variables
+
+	// Random Number used to generate the next color
+	private int num; 
+
+	// Time interval between colors. Default is half second, shortens as time goes on...
+	private float time; 
+
+	// Public text variables used to display small messages and the score.
+	public Text score, message;
+
+	// Is the color pattern currently being played back? Used to lock controls.
+	private bool playingBack; 
+
+	// Various sounds to play.
+	AudioSource green, red, yellow, blue, winsound, losesound; 
+
+	// Various lights to flash at certain times.
+	public Light green1, green2, green3, red1, red2, red3, yellow1, yellow2, yellow3, blue1, blue2, blue3; 
+
+	// This list is used to store the sequence of colors that will play. 
 	List<string> sequence = new List<string>();
 
-	int num; // Random Number used to generate the next color
-	float time; // Time interval between colors. Default is half second.
-	public Text score, message;
-	private bool playingBack; // Is the color pattern currently being played back?
-
-	AudioSource green, red, yellow, blue, winsound, losesound; // Various sounds
-	public Light green1, green2, green3, red1, red2, red3, yellow1, yellow2, yellow3, blue1, blue2, blue3; // All the lights.
-
+	// The list that the user will be actively typing into to copy the pattern.
 	private List <KeyCode> def = new List<KeyCode>() {};
-	private List <KeyCode> correct; // The sequence that correlates to what order the keys need to be pressed in.
-	private int correctIndex; // The index counter for what order keys are pressed in.
 
+	// The sequence that correlates to what order the keys need to be pressed in. Converted from sequence list to KeyCode for input.
+	private List <KeyCode> correct; 
 
-	// Use this for initialization
+	// The index counter for what order keys are pressed in.
+	private int correctIndex; 
+
+	#endregion variables
+
+	// Initilizes sound and text, then starts the opening sequence after 1 second.
 	void Start () {
 		correct = def;
 		AudioSource[] audios = GetComponents<AudioSource> ();
@@ -42,18 +61,21 @@ public class MainDriver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (playingBack);
-
-		setTime ();
-
+		
+		// Only allow input if the game is not playing back a pattern.
 		if (!playingBack) {
+
+			// When the player gets a correct press...
 			if (Input.GetKeyDown (correct [correctIndex])) {
+
+				// If the player finishes the entire color sequences, reset the index and notify the player is correct.
 				if (++correctIndex == correct.Count) {
 					correctIndex = 0;
-					print ("Correct!");
-					//Correct - do whatever is next
+					//print ("Correct!");
 					Invoke ("Win", time);
 				}
+
+				// Light up depending on the color.
 				if (Input.GetKeyDown (KeyCode.LeftArrow))
 					Yellow ();
 				if (Input.GetKeyDown (KeyCode.RightArrow))
@@ -62,19 +84,30 @@ public class MainDriver : MonoBehaviour {
 					Green ();
 				if (Input.GetKeyDown (KeyCode.DownArrow))
 					Blue ();
-			
-			} else if (Input.GetKeyDown (KeyCode.P)) {
-				if (!playingBack)
-					StartCoroutine (PlayGame ());
-			} else if (Input.GetKeyDown (KeyCode.Q)) {
+			} 
+
+			// If the player hits "P", redisplay the pattern.
+			else if (Input.GetKeyDown (KeyCode.P)) {
+				StartCoroutine (PlayGame ());
+			} 
+
+			// If the player hits Q, effectively restart the scene to reload the game.
+			else if (Input.GetKeyDown (KeyCode.Q)) {
 				SceneManager.LoadScene ("Simon");
-			} else if (Input.GetKeyDown (KeyCode.Escape))
+			} 
+
+			// If the player hits Escape, exit game.
+			else if (Input.GetKeyDown (KeyCode.Escape))
 				Application.Quit ();
+
+			// If any other key is pressed, just play error sound, and reset the index. 
 			else if (Input.anyKeyDown) {
 				correctIndex = 0;
 				print ("Wrong!");
 				//The user is wrong - game over.
 				Invoke ("Lose", time);
+
+				// If they hit a wrong arrow key, still light up for feedback.
 				if (Input.GetKeyDown (KeyCode.LeftArrow))
 					Yellow ();
 				if (Input.GetKeyDown (KeyCode.RightArrow))
@@ -114,7 +147,7 @@ public class MainDriver : MonoBehaviour {
 		StartCoroutine (PlayGame ());
 	} 
 
-	// What happens when you win
+	// Called when you make a correct sequence. Calls to add a new color, plays sound, and locks controls.
 	void Win() {
 		winsound.Play ();
 		message.text = "Correct!";
@@ -124,14 +157,14 @@ public class MainDriver : MonoBehaviour {
 		playingBack = true;
 	}
 
-	// What happens when you lose
+	// If you mess up play a lose sound, and tell the user they are wrong.
 	void Lose() {
 		losesound.Play ();
 		message.text = "Wrong!";
 		//TextUpdate ();
 	}
 
-	// Runs the game
+	// Plays back the pattern to the user.
 	IEnumerator PlayGame() {
 
 		playingBack = true;
@@ -152,12 +185,14 @@ public class MainDriver : MonoBehaviour {
 		playingBack = false;
 	} 
 
-	//Sets the Score Text as needed
+	// Sets the Score Text as needed, and calls to check the time difficulty.
 	void TextUpdate () {
 		score.text = "Score: " + (sequence.Count).ToString ();
+		setTime ();
 		StartCoroutine (MessageDisplay());
 	}
 
+	// Makes the "Correct!" and "Wrong!" messages only stay for 2 seconds, as well as the "Game Start!" at the beginning.
 	IEnumerator MessageDisplay() {
 		message.enabled = true;
 		yield return new WaitForSeconds (2);
